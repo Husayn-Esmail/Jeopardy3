75cfdca73cbe967c7ccbc21c369df93394d50cda
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-01-17 14:40:00 -0700

    created a couple of classes, applied some basic styling and successfully displayed categories responsively

--- jeopardy_3/jeopardy_app/static/jeopardy/js/main.js
+++ jeopardy_3/jeopardy_app/static/jeopardy/js/main.js
@@ -7,15 +7,23 @@ class Category {
 }
 
 class Question {
-
+    contructor(q, a, val) {
+        this.q = q
+        this.a = a
+        this.val = val
+    }
 }
 
 
 
 for (var datum in data) {
     console.log(datum)
+    let p = document.createElement('p')
+    p.innerHTML = datum
+    p.className = 'category'
     var extract = data[datum]
     for (var dat in data[datum]) {
         console.log(extract[dat]);
     }
+    document.getElementById('cats').append(p)
 }
