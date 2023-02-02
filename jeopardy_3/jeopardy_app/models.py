fcb2aefa5e167bea4adf16bea8c9eb9bff6f1f7a
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-02-02 13:07:20 -0700

    added the comment to models

--- jeopardy_3/jeopardy_app/models.py
+++ jeopardy_3/jeopardy_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Jeopardy_Question(models.Model):
+    """
+    Tasks:
+    construct the model in such a way that it adapts the
+    read_questions.py method. should still have the ability
+    to use a CSV to create questions
+    """
+    pass
