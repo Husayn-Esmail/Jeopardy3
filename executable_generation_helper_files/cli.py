ca46f20c0ad59ba5cff63a441bc56da5f7f17797
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-09-14 15:54:53 -0600

    moved the executable generation files into a separate folder becuase i'm parking that for now

---
+++ executable_generation_helper_files/cli.py
@@ -0,0 +1,7 @@
+# what if this file runs the server and opens the webpage in the user's default browser
+
+# from django.core.management import call_command
+# import jeopardy_3.wsgi
+# if __name__ == '__main__':
+#     application = jeopardy_3.wsgi.application
+#     call_command("runserver", "0.0.0.0:8000")
