cb35b9d27c93d5c144707baa43218744eb743097
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2023-09-17 19:51:15 -0600

    changed entry point in package.json and corrected typo

--- jeopardy_3/jeopardy_frontend/public/preload.js
+++ jeopardy_3/jeopardy_frontend/public/preload.js
@@ -1,6 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge = exposeInMainWordl('versions', {
+contextBridge = exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
